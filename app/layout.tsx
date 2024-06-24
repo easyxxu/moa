7948a0f5bf60a13eabc31eafbc28f61e4977d604
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-24 20:50:32 +0900

    Style: font 설정 #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { GeistSans } from "geist/font/sans";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -11,13 +11,20 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
+const pretendard = localFont({
+  src: "../styles/font/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en" className={`${pretendard.variable}`}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ["var(--font-pretendard)"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
