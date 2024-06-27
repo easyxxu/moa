a846e3ff9a3a5ef52b8e2c65aeb587433fd10421
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-27 11:22:31 +0900

    Feat: product type 설정

---
+++ types/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: number;
+  name: string;
+  desc: string;
+  price: number;
+  image: string;
+  stock: number;
+  seller: number;
+  store_name: string;
+  created_at: string;
+  updated_at: string;
+  shipping_fee: number;
+}
