81beb057c25f0e6ff2d514a0a9991974a2b83c06
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-27 11:21:43 +0900

    Style: 메인 layout 스타일 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${pretendard.variable}`}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+      <body className="bg-background">
+        <div className="min-h-screen flex flex-col items-center">
           {children}
-        </main>
+        </div>
       </body>
     </html>
   );
