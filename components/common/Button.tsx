fd51e15ba4d5dcc134e60597b3d20702716df00c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-06-28 17:18:26 +0900

    Style: Button 컴포넌트 custom 속성 추가

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -4,12 +4,19 @@ interface ButtonProps {
   type?: "submit" | "button";
   src?: string;
   alt?: string;
+  custom?: string;
   children: React.ReactNode;
 }
 
-export default function Button({ type, src, alt, children }: ButtonProps) {
+export default function Button({
+  type,
+  src,
+  alt,
+  custom,
+  children,
+}: ButtonProps) {
   return (
-    <button type={type} className="button">
+    <button type={type} className={`${custom} outer-box`}>
       {/* <Image src={src} alt={alt} /> */}
       {children}
     </button>
