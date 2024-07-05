e314309ed8b749d05a6eae4d73ffa59a6733ce06
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-05 16:35:30 +0900

    Feat: 회원가입 구현 #2 - server action 적용 - InputLabel style, error props 적용

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,15 +1,47 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import Form from "@/components/authForm/Form";
 import InputLabel from "@/components/common/InputLabel";
 
+import { joinAction } from "@/actions/actions";
+
 export default function Join() {
+  const [state, formAction] = useFormState(joinAction, {});
+
   return (
-    <Form formType="join">
-      <form className="flex flex-col gap-4">
-        <InputLabel name="이메일" label="email" style="box" />
-        <InputLabel name="비밀번호" label="password" style="box" />
-        <InputLabel name="이름" label="name" style="box" />
-        <InputLabel name="전화번호" label="phoneNum" style="box" />
-      </form>
+    <Form formType="join" onSubmit={formAction}>
+      <InputLabel
+        name="이메일"
+        label="email"
+        type="text"
+        style="box"
+        error={state.errorMsg?.email}
+      />
+      <InputLabel
+        name="비밀번호"
+        label="password"
+        type="password"
+        style="box"
+        error={state.errorMsg?.password}
+      />
+      <InputLabel
+        name="이름"
+        label="name"
+        type="text"
+        style="box"
+        error={state.errorMsg?.name}
+      />
+      <InputLabel
+        name="전화번호"
+        label="phone"
+        type="tel"
+        placeholder="-은 제외하고 입력해주세요."
+        style="box"
+        error={state.errorMsg?.phone}
+      />
+      <span className="text-sm text-red-400">{state.errorMsg?.server}</span>
     </Form>
   );
 }
