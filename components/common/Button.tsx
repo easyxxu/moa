e956a2c905545e367bc069d3ecd6c8ef793a8344
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-09 10:24:48 +0900

    Style: 사용 안하는 prop 삭제

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,25 +1,18 @@
-import Image from "next/image";
-
 interface ButtonProps {
-  type?: "submit" | "button";
-  src?: string;
-  alt?: string;
+  type: "submit" | "button";
   custom?: string;
-  onClick: React.MouseEventHandler;
+  onClick?: React.MouseEventHandler;
   children: React.ReactNode;
 }
 
 export default function Button({
   type,
-  src,
-  alt,
   custom,
   onClick,
   children,
 }: ButtonProps) {
   return (
     <button type={type} className={`${custom} outer-box`} onClick={onClick}>
-      {/* <Image src={src} alt={alt} /> */}
       {children}
     </button>
   );
