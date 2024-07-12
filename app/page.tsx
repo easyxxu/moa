a28ea04a6560742402d29657111a4beb8b494979
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-12 22:28:27 +0900

    Style: 홈 메인 컨텐츠 중앙 정렬 #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { createClient } from "@/utils/supabase/server";
+
 import Header from "@/components/Header";
 import CardList from "@/components/card/CardList";
+import Footer from "@/components/common/Footer";
+
 import { Product } from "@/types/product";
 
 export default async function Index() {
@@ -8,27 +11,12 @@ export default async function Index() {
   const { data: products } = await supabase.from("products").select();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="">
       <Header />
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6">
-          <CardList products={products || ([] as Product[])} />
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-pretendard font-black hover:underline"
-            rel="noreferrer"
-          >
-            열심히 만드는 중
-          </a>
-        </p>
-      </footer>
+      <main className="mx-auto my-4 max-w-7xl">
+        <CardList products={products || ([] as Product[])} />
+      </main>
+      <Footer />
     </div>
   );
 }
