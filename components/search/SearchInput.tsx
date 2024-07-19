0d5d41690b93c1c3546f1b4748ee375eb0981aa6
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-19 11:48:02 +0900

    Style: 검색 페이지 이동 라우터 설정

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,10 +1,13 @@
+"use client";
 import Image from "next/image";
 import Button from "../common/Button";
 import SearchIcon from "@/public/assets/icon/icon-search.svg";
+import { useRouter } from "next/navigation";
 
 export default function SearchInput() {
+  const router = useRouter();
   return (
-    <form className="flex gap-3">
+    <form className="flex gap-3 mr-2">
       <div className="w-80 min-w-30 h-12 p-3 rounded-xl shadow-in">
         <label className="w-1/3">
           <input
@@ -14,7 +17,11 @@ export default function SearchInput() {
           />
         </label>
       </div>
-      <Button custom="w-12 h-12">
+      <Button
+        type="submit"
+        custom="w-12 h-12"
+        onClick={() => router.push("/search")}
+      >
         <Image src={SearchIcon} alt="검색" />
       </Button>
     </form>
