8c238e21f80f7191654ab56bf6fc38fc2a1b8166
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-19 11:48:39 +0900

    Style: onBlur 속성 추가

--- components/common/InputLabel.tsx
+++ components/common/InputLabel.tsx
@@ -5,6 +5,7 @@ interface InputLabelProps {
   placeholder?: string;
   style: "box" | "line";
   error?: string;
+  onBlur?: React.ChangeEventHandler;
 }
 
 export default function InputLabel({
@@ -14,9 +15,10 @@ export default function InputLabel({
   placeholder,
   style,
   error,
+  onBlur,
 }: InputLabelProps) {
   return (
-    <div className="flex flex-col gap-1 font-extralight text-xl">
+    <div className="flex flex-col gap-1 font-extralight">
       {style === "box" ? (
         <>
           <label htmlFor={label}>{name}</label>
@@ -24,9 +26,10 @@ export default function InputLabel({
             id={label}
             name={label}
             type={type}
-            className="inner-box px-4 py-2"
+            className="px-4 py-2 inner-box"
             placeholder={placeholder}
             required
+            onBlur={onBlur}
           />
           <span className="text-sm text-red-600">{error}</span>
         </>
@@ -37,13 +40,14 @@ export default function InputLabel({
               id={label}
               name={label}
               type={type}
-              className="peer w-full border-b-2 bg-inherit px-2 py-2 focus:outline-none focus:border-b-font-hover"
+              className="w-full px-2 py-2 border-b-2 peer bg-inherit focus:outline-none focus:border-b-font-hover"
               placeholder={placeholder}
               required
+              onBlur={onBlur}
             />
             <label
               htmlFor={label}
-              className="absolute left-2 bottom-2 text-md transition-all peer-focus:text-sm peer-focus:bottom-9 peer-valid:text-sm peer-valid:bottom-9"
+              className="absolute transition-all left-2 bottom-2 text-md peer-focus:text-sm peer-focus:bottom-9 peer-valid:text-sm peer-valid:bottom-9"
             >
               {name}
             </label>
