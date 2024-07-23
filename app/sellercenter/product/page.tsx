479aa01be16726f965dfa629cbf18c2d52ab98d5
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-07-23 18:02:48 +0900

    Style: 판매자센터-상품 리스트

--- app/sellercenter/product/page.tsx
+++ app/sellercenter/product/page.tsx
@@ -1,16 +1,83 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import TableHeader from "@/components/table/TableHeader";
+import TableCell from "@/components/table/TableItem";
+import PlusIcon from "@/public/assets/icon/icon-plus-round.svg";
+
+import { Product } from "@/types/product";
+import { useUserState } from "@/contexts/UserContext";
+
+import { createClient } from "@/utils/supabase/client";
+
+const listHeaders = ["상품명", "가격", "수량", "수정", "삭제"];
 
 export default function ProductDashboard() {
+  const supabase = createClient();
+  const router = useRouter();
+  const userState = useUserState();
+  const [productData, setProductData] = useState<Product[]>([]);
+  const userId = userState.id;
+
+  const handleDeleteProduct = () => {
+    console.log("product deleted");
+  };
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data, error } = await supabase
+        .from("product")
+        .select()
+        .eq("seller_id", userId);
+
+      if (error) {
+        console.error(error);
+      }
+
+      setProductData(data ?? []);
+    };
+    if (userId) fetchProduct();
+  }, []);
+
   return (
-    <div>
-      <div>
-        <Link
-          href="/sellercenter/product/add"
-          className="px-3 py-2 text-xl font-semibold shadow-out rounded-3xl bg-primary"
-        >
-          상품 등록
-        </Link>
-      </div>
+    <div className="w-full">
+      <Link
+        href="/sellercenter/product/add"
+        className="mb-3 flex justify-center gap-2 px-3 py-2 text-2xl items-center font-medium shadow-out rounded-2xl bg-primary hover:shadow-in"
+      >
+        <Image
+          src={PlusIcon}
+          alt=""
+          className="w-6 h-6"
+          width={30}
+          height={30}
+        />
+        상품 등록
+      </Link>
+      <table className="w-full shadow-out">
+        <TableHeader headers={listHeaders} />
+        <tbody>
+          {productData.map((product) => (
+            <tr>
+              <TableCell.TextWithImgCell
+                src={product.image[0]}
+                text={product.name}
+              />
+              <TableCell.TextCell text={product.name} />
+              <TableCell.TextCell text={product.price} />
+              <TableCell.ButtonCell
+                text="수정"
+                onClick={() => router.push("/sellercenter/modify")}
+              />
+              <TableCell.ButtonCell text="삭제" onClick={handleDeleteProduct} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
