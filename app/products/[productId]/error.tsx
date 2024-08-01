3fcdad3f904c56e6ae52927a666742e2405073ee
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-08-01 17:40:49 +0900

    Feat: 상품 상세 페이지 에러 페이지

--- app/products/[productId]/error.tsx
+++ app/products/[productId]/error.tsx
@@ -1,14 +1,33 @@
 "use client";
 
+import { useEffect } from "react";
 import Image from "next/image";
-import ErrorIcon from "@/public/assets/icon/icon-error.svg";
 
-export default function ErrorPage() {
+import ErrorIcon from "@/public/assets/icon/icon-error.svg";
+import Button from "@/components/common/button/Button";
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
-    <div className="flex flex-col items-center justify-center mt-40">
+    <div className="flex flex-col gap-1 items-center justify-center h-screen">
       <Image src={ErrorIcon} alt="오류" />
-      <p>데이터를 가져오는데 오류가 발생했습니다.</p>
-      <p>잠시 후에 다시 시도해주세요.</p>
+      <p className="text-lg">데이터를 가져오는데 오류가 발생했습니다.</p>
+      <Button
+        onClick={() => reset()}
+        type="button"
+        custom="px-3 py-2 bg-primary font-semibold text-xl"
+      >
+        다시 시도하기
+      </Button>
     </div>
   );
 }
