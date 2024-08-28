9970b88c31df0808b8778185b7e51a88a5769236
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-08-28 18:25:24 +0900

    Feat: cart 관련 api, 상품 id별 데이터 가져오기

--- api/apis.ts
+++ api/apis.ts
@@ -1,9 +1,9 @@
 "use server";
 
+import { CartItemInfo, GroupedCartItems } from "@/types/cart";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-
-const supabase = createClient();
+import { revalidatePath } from "next/cache";
 
 interface ProductForm {
   name: string;
@@ -14,6 +14,7 @@ interface ProductForm {
 }
 
 export async function addProduct(formData: ProductForm) {
+  const supabase = createClient();
   const image = formData.image;
   // console.log("#입력된 formData: ", formData);
 
@@ -28,7 +29,32 @@ export async function addProduct(formData: ProductForm) {
   redirect("/sellercenter/product");
 }
 
+export async function loadProductById(productId: number) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("product")
+    .select(
+      "name, price, description, shipping_fee, stock, image, seller_store"
+    )
+    .eq("id", productId)
+    .single();
+
+  if (error) {
+    // console.log("상품 ID별 정보를 불러오는데 실패했습니다.", error);
+    throw new Error(
+      `상품 ID별 정보를 불러오는데 실패했습니다. 
+      메시지: ${error.message}
+      코드: ${error.code}
+      힌트: ${error.hint}
+      세부 사항: ${error.details}`
+    );
+  }
+
+  return { data };
+}
+
 export async function updateProduct(formData: any, productId: number) {
+  const supabase = createClient();
   const { error } = await supabase
     .from("product")
     .update({ ...formData })
@@ -36,3 +62,114 @@ export async function updateProduct(formData: any, productId: number) {
 
   return error;
 }
+
+export async function createCart(userId: string) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("cart")
+    .insert({ user_id: userId })
+    .select()
+    .single();
+
+  return data.id;
+}
+
+export async function getCartId(userId: string) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("cart")
+    .select("id")
+    .eq("user_id", userId)
+    .single();
+  // if (!data) console.log("no data");
+  return data?.id;
+}
+
+interface CartItem {
+  cartId: number;
+  productId: number;
+  quantity: number;
+}
+export async function addCartItem(cartItem: CartItem) {
+  const supabase = createClient();
+  const { cartId, productId, quantity } = cartItem;
+
+  const { data, error } = await supabase
+    .from("cart_item")
+    .insert({ cart_id: cartId, product_id: productId, quantity })
+    .select();
+
+  revalidatePath("/cart");
+
+  return { data, error };
+}
+
+export async function checkCartItem(cartId: number, productId: number) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("cart_item")
+    .select()
+    .eq("cart_id", cartId)
+    .eq("product_id", productId);
+
+  if (data!.length > 0) {
+    return true;
+  }
+}
+
+export async function getCartItem(cartId: number) {
+  const supabase = createClient();
+
+  const { data: cartItems, error } = await supabase
+    .from("cart_item")
+    .select()
+    .eq("cart_id", cartId);
+
+  /**
+   * TODO 장바구니에 담긴 아이템이 없는 경우 처리가 필요함(현재 처리는 임시임)
+   */
+  if (!cartItems) return;
+
+  /**
+   ** 상품정보+수량을 같이 리턴함
+   */
+  const cartItemsInfo = await Promise.all(
+    cartItems?.map(
+      async (cartItem: { product_id: number; quantity: number }) => {
+        const { data: productInfo } = await loadProductById(
+          cartItem["product_id"]
+        );
+
+        return {
+          id: cartItem["product_id"],
+          quantity: cartItem["quantity"],
+          ...productInfo,
+        };
+      }
+    )
+  );
+
+  const groupBySellerStore = (cartItems: CartItemInfo[]): GroupedCartItems => {
+    return cartItems.reduce((acc: GroupedCartItems, item: CartItemInfo) => {
+      const { seller_store } = item;
+      if (!acc[seller_store]) {
+        acc[seller_store] = [];
+      }
+      acc[seller_store].push(item);
+      return acc;
+    }, {});
+  };
+
+  return groupBySellerStore(cartItemsInfo);
+}
+
+export const updateQuantity = async (productId: number, quantity: number) => {
+  const supabase = createClient();
+  const { data } = await supabase
+    .from("cart_item")
+    .update({ quantity: quantity })
+    .eq("product_id", productId)
+    .select()
+    .single();
+  return data;
+};
