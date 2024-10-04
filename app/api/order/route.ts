efc720d4cd6968f53bd41c676171161ec19e6f88
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-04 18:42:54 +0900

    Feat: order api(post) order_item 추가로 인한 수정 #14 - 주문 상품 정보를 담은 order_item 테이블 추가로 인해 수정

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,3 +1,4 @@
+import { CartItem } from "@/contexts/CartContext";
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -8,26 +9,48 @@ export async function POST(req: Request) {
     const data = await req.json();
     console.log("POST request data:", data);
 
-    const { error } = await supabase.from("order").insert({
-      order_name: data.orderName,
-      order_status: "PAYMENT_PENDING",
-      customer_name: data.name,
-      customer_email: data.email,
-      customer_phone: data.phone,
-      customer_address: `${data.postCode}, ${data.defaultAddress} ${data.detailAddress}`,
-      customer_delivery_message: data.deliveryMessage,
-      payment_status: "PAYMENT_IN_PROCESS",
-      payment_method: data.payment,
-      order_items: data.orderItems,
-      total_price: data.totalPrice,
-    });
-    if (error) {
+    const { data: orderData, error: orderError } = await supabase
+      .from("order")
+      .insert({
+        order_name: data.orderName,
+        order_status: "PAYMENT_PENDING",
+        customer_name: data.name,
+        customer_email: data.email,
+        customer_phone: data.phone,
+        customer_address: `${data.postCode}, ${data.defaultAddress} ${data.detailAddress}`,
+        customer_delivery_message: data.deliveryMessage,
+        payment_status: "PAYMENT_IN_PROCESS",
+        payment_method: data.payment,
+        total_price: data.totalPrice,
+      })
+      .select();
+
+    if (orderError || !orderData) {
       return NextResponse.json({
-        status: error.code,
-        message: "Order created Failed",
-        error,
+        status: orderError?.code || 500,
+        message: "Order creation failed",
+        error: orderError,
       });
     }
+
+    const orderId = orderData[0].id;
+
+    await Promise.all(
+      data.orderItems.map(async (item: CartItem) => {
+        const { error: orderItemError } = await supabase
+          .from("order_item")
+          .insert({
+            order_id: orderId,
+            item_id: item.itemId,
+            quantity: item.quantity,
+            price: item.price,
+            shipping_fee: item.shipping_fee,
+          });
+        if (orderItemError) {
+          console.log("Order item insertion failed: ", orderItemError);
+        }
+      })
+    );
     return NextResponse.json({
       status: 200,
       message: "Order created successfully",
