73541c0cb7b7ab3ee962804d2aa59ca954de46d5
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-11 19:01:53 +0900

    Feat: product 정렬 순서 id 순 적용

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,18 @@ import Header from "@/components/common/Header";
 import CardList from "@/components/card/CardList";
 import Footer from "@/components/common/Footer";
 
-import { Product } from "@/types/product";
-
 export default async function Index() {
   const supabase = createClient();
-  const { data: products } = await supabase.from("product").select();
+  const { data: products } = await supabase
+    .from("product")
+    .select()
+    .order("id");
 
   return (
     <div className="">
       <Header />
       <main className="mx-auto my-4 max-w-7xl">
-        <CardList products={products || ([] as Product[])} />
+        <CardList products={products!} />
       </main>
       <Footer />
     </div>
