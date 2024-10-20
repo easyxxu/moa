903dca58d9e4b067887a81d01774ff82ab624e06
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-20 13:19:08 +0900

    Style: CardItem 재사용하도록 수정 #17

--- app/mypage/review/[orderItemId]/write/[productId]/page.tsx
+++ app/mypage/review/[orderItemId]/write/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import { loadProductById } from "@/api/apis";
-import ReviewForm from "@/components/review/ReviewForm";
-import Image from "next/image";
+import CardItem from "@/components/card/CardItem";
+import ReviewForm from "@/components/mypage/reviewWrite/ReviewForm";
 
 export default async function ReviewWrite({
   params,
@@ -12,22 +12,15 @@ export default async function ReviewWrite({
 
   return (
     <div className="w-full">
-      <h2 className="text-2xl font-semibold text-center">리뷰 작성하기</h2>
-      <p className="mb-2 text-xl font-medium">상품은 어떠셨나요?</p>
-      <div className="flex gap-8">
-        <div>
-          <div className="w-64 shadow-out rounded-xl">
-            <Image
-              src={res.data.image[0]}
-              alt={`${res.data.name} 이미지`}
-              width={300}
-              height={300}
-              className="rounded-t-xl"
-            />
-            <p className="px-4 py-3 bg-white rounded-b-xl font-extralight">
-              {res.data.name}
-            </p>
-          </div>
+      <h2 className="text-2xl font-semibold">리뷰 작성하기</h2>
+      <p className="mb-2 text-xl font-medium text-center">상품은 어떠셨나요?</p>
+      <div className="flex flex-col items-center gap-8 w-full">
+        <div className="w-64">
+          <CardItem
+            id={res.data.id}
+            src={res.data.image[0]}
+            name={res.data.name}
+          />
         </div>
         <ReviewForm />
       </div>
