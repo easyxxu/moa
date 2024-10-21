5481497f5436f2a59548ef2dd2b0235c7533286b
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-22 01:13:02 +0900

    Feat: 이미지 클릭 시 미리보기 컴포넌트 구현 #19

---
+++ components/common/ImagePreview.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import ArrowIcon from "@/public/assets/icon/icon-arrow.svg";
+import CloseIcon from "@/public/assets/icon/icon-delete.svg";
+import { useState } from "react";
+
+interface Props {
+  images: string[];
+  onClose: () => void;
+}
+export default function ImagePreview({ images, onClose }: Props) {
+  const [imageOrder, setImageOrder] = useState(0);
+
+  const handlePrevious = () => {
+    setImageOrder((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+  const handleNext = () => {
+    setImageOrder((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+  return (
+    <div className="fixed top-0 left-0 z-10 w-full h-full bg-background-modal">
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="h-3/4">
+          <Image
+            src={images[imageOrder]}
+            alt="상품 이미지"
+            width={300}
+            height={300}
+            className="translate-y-1/4"
+          />
+        </div>
+        <div className="flex items-center justify-center gap-10">
+          <button type="button" onClick={handlePrevious}>
+            <Image src={ArrowIcon} alt="이전 이미지" width={30} height={30} />
+          </button>
+          <p className="text-lg font-light text-center text-white">
+            {imageOrder + 1} / {images.length}
+          </p>
+          <button type="button" onClick={handleNext}>
+            <Image
+              src={ArrowIcon}
+              alt="다음 이미지"
+              width={30}
+              height={30}
+              className="rotate-180"
+            />
+          </button>
+        </div>
+      </div>
+      <button
+        type="button"
+        className="absolute top-10 right-10"
+        onClick={onClose}
+      >
+        <Image src={CloseIcon} alt="닫기" width={30} height={30} />
+      </button>
+    </div>
+  );
+}
