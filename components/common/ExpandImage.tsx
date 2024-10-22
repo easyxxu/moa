b19d0f68d40fff8a81eba7492ec68c1661af37da
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-22 11:08:40 +0900

    Feat: 클릭한 사진을 먼저 보여주도록 수정 #19

--- components/common/ExpandImage.tsx
+++ components/common/ExpandImage.tsx
@@ -7,10 +7,11 @@ import { useState } from "react";
 
 interface Props {
   images: string[];
+  order: number;
   onClose: () => void;
 }
-export default function ExpandImage({ images, onClose }: Props) {
-  const [imageOrder, setImageOrder] = useState(0);
+export default function ExpandImage({ images, order, onClose }: Props) {
+  const [imageOrder, setImageOrder] = useState(order);
 
   const handlePrevious = () => {
     setImageOrder((prev) => (prev === 0 ? images.length - 1 : prev - 1));
@@ -21,20 +22,19 @@ export default function ExpandImage({ images, onClose }: Props) {
   return (
     <div className="fixed top-0 left-0 z-10 w-full h-full bg-background-modal">
       <div className="flex flex-col items-center justify-center h-full">
-        <div className="h-3/4">
+        <div className="flex items-center h-3/4">
           <Image
             src={images[imageOrder]}
             alt="상품 이미지"
-            width={300}
-            height={300}
-            className="translate-y-1/4"
+            width={400}
+            height={400}
           />
         </div>
         <div className="flex items-center justify-center gap-10">
           <button type="button" onClick={handlePrevious}>
             <Image src={ArrowIcon} alt="이전 이미지" width={30} height={30} />
           </button>
-          <p className="text-lg font-light text-center text-white">
+          <p className="w-20 text-lg font-light text-center text-white">
             {imageOrder + 1} / {images.length}
           </p>
           <button type="button" onClick={handleNext}>
