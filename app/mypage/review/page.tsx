a90364cd1d242a393fba3d4207669a134217eb1c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-22 15:31:41 +0900

    Feat: 리뷰 불러오기 조건 추가 #18 - 로그인한 유저가 작성한 리뷰만 불러오도록 조건 추가

--- app/mypage/review/page.tsx
+++ app/mypage/review/page.tsx
@@ -3,9 +3,25 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function ReviewPage() {
   const supabase = createClient();
-  const { data, error } = await supabase.from("review").select(`
+  const { data: user, error: userError } = await supabase.auth.getUser();
+  if (userError) {
+    console.error(
+      "리뷰를 불러오기 위한 사용자 정보를 불러오는데 실패했습니다.",
+      userError
+    );
+    throw new Error("서버에서 문제가 생겼습니다.");
+  }
+  const userId = user.user?.id;
+
+  const { data, error } = await supabase
+    .from("review")
+    .select(
+      `
     *, product(id, name, image, seller_store, seller_id)
-  `);
+  `
+    )
+    .eq("user_id", userId!);
+
   if (!data || data.length === 0) return <p>작성한 리뷰가 없습니다.</p>;
   if (error) {
     console.error("리뷰를 불러오는데 실패했습니다.", error);
