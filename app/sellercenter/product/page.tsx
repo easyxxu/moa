3cd200f1ae87cd203249b36c759a3d8a12d5ccc8
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-31 18:09:59 +0900

    Style: 스타일 수정, 상품 0개인 경우 처리

--- app/sellercenter/product/page.tsx
+++ app/sellercenter/product/page.tsx
@@ -9,10 +9,10 @@ import TableHeader from "@/components/table/TableHeader";
 import TableCell from "@/components/table/TableItem";
 import PlusIcon from "@/public/assets/icon/icon-plus-round.svg";
 
-import { Product } from "@/types/product";
 import { useUserState } from "@/contexts/UserContext";
 
 import { createClient } from "@/utils/supabase/client";
+import { Tables } from "@/types/database.types";
 
 const listHeaders = ["상품명", "가격", "수량", "수정", "삭제"];
 
@@ -20,7 +20,7 @@ export default function ProductDashboard() {
   const supabase = createClient();
   const router = useRouter();
   const userState = useUserState();
-  const [productData, setProductData] = useState<Product[]>([]);
+  const [productData, setProductData] = useState<Tables<"product">[]>([]);
   const userId = userState.id;
 
   const handleDeleteProduct = async (id: number) => {
@@ -34,57 +34,62 @@ export default function ProductDashboard() {
       const { data, error } = await supabase
         .from("product")
         .select()
-        .eq("seller_id", userId);
+        .eq("seller_id", userId!);
 
       if (error) {
         console.error(error);
       }
 
-      setProductData(data ?? []);
+      setProductData(data!);
     };
     if (userId) fetchProduct();
   }, []);
 
   return (
-    <div className="w-full">
-      <Link
-        href="/sellercenter/product/add"
-        className="mb-3 flex justify-center gap-2 px-3 py-2 text-2xl items-center font-medium shadow-out rounded-2xl bg-primary hover:shadow-in"
-      >
-        <Image
-          src={PlusIcon}
-          alt=""
-          className="w-6 h-6"
-          width={30}
-          height={30}
-        />
-        상품 등록
+    <div className="w-full bg-white shadow-md">
+      <Link href="/sellercenter/product/add">
+        <div className="flex items-center justify-center gap-2 px-3 py-4 font-semibold text-white transition-colors bg-gray-300 hover:bg-gray-400">
+          <Image
+            src={PlusIcon}
+            alt="상품 등록 아이콘"
+            className="w-6 h-6"
+            width={30}
+            height={30}
+          />
+          상품 등록
+        </div>
       </Link>
-      <table className="w-full shadow-out">
-        <TableHeader headers={listHeaders} />
-        <tbody>
-          {productData.map((product) => (
-            <tr key={product.id} className="bg-white border-b">
-              <TableCell.TextWithImgCell
-                src={product.image[0]}
-                text={product.name}
-              />
-              <TableCell.TextCell text={product.price.toLocaleString()} />
-              <TableCell.TextCell text={product.stock} />
-              <TableCell.ButtonCell
-                text="수정"
-                onClick={() =>
-                  router.push(`/sellercenter/product/modify/${product.id}`)
-                }
-              />
-              <TableCell.ButtonCell
-                text="삭제"
-                onClick={() => handleDeleteProduct(product.id!)}
-              />
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {productData.length === 0 ? (
+        <div className="flex items-center justify-center h-40 text-lg font-semibold text-gray-500">
+          등록된 상품이 없습니다.
+        </div>
+      ) : (
+        <table className="w-full">
+          <TableHeader headers={listHeaders} />
+          <tbody>
+            {productData.map((product) => (
+              <tr key={product.id} className="bg-white border-b">
+                <TableCell.TextWithImgCell
+                  src={product.image[0]}
+                  text={product.name}
+                />
+                <TableCell.TextCell text={product.price.toLocaleString()} />
+                <TableCell.TextCell text={product.stock} />
+                <TableCell.ButtonCell
+                  text="수정"
+                  onClick={() =>
+                    router.push(`/sellercenter/product/modify/${product.id}`)
+                  }
+                />
+                <TableCell.ButtonCell
+                  text="삭제"
+                  onClick={() => handleDeleteProduct(product.id!)}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
