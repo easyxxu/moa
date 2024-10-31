24d1cb93e82ee624a9017be128b362e85020abc3
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-31 22:55:52 +0900

    Style: 상품 정보 타이틀 생성 날짜로 변경

--- components/mypage/OrderItem.tsx
+++ components/mypage/OrderItem.tsx
@@ -5,27 +5,11 @@ interface Props {
   order: OrderWithOrderItem;
 }
 export default function OrderItem({ order }: Props) {
-  const dateStr = order.order_name.split("_")[0];
-  const timeStr = order.order_name.split("_")[1];
-
-  const year = dateStr.slice(0, 4);
-  const month = dateStr.slice(4, 6);
-  const day = dateStr.slice(6, 8);
-
-  const hours = timeStr.slice(0, 2);
-  const minutes = timeStr.slice(2, 4);
-  const seconds = timeStr.slice(4, 6);
-
-  const orderTitleDate = `${year}.${month}.${day}`;
-  const orderTitleTime = `${hours}:${minutes}:${seconds}`;
   return (
-    <div className="my-4">
+    <div className="px-2 py-3">
       <div className="flex items-center gap-2 mb-4">
-        <p className="text-lg font-light">
-          <strong className="mr-1 text-xl font-semibold ">
-            {orderTitleDate}
-          </strong>
-          {orderTitleTime}
+        <p className="text-lg font-medium">
+          {new Date(order.created_at).toLocaleString()}
         </p>
       </div>
       <ul>
