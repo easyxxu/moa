e5ecb2a7205cdb2c12b5b3da4b78d00a733849c0
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-31 16:32:53 +0900

    Style: 판매자센터 메뉴 스타일 수정 - 기존보다 깔끔하게 변경

--- components/sellercenter/MenuItem.tsx
+++ components/sellercenter/MenuItem.tsx
@@ -22,20 +22,18 @@ export default function MenuItem({
   return (
     <Link href={`/sellercenter${path}`}>
       <div
-        className={`flex items-center justify-between p-4 font-regular rounded-2xl whitespace-nowrap ${
-          isActive
-            ? "bg-primary shadow-in font-semibold"
-            : "bg-inherit shadow-out"
-        } hover:bg-primary`}
+        className={`flex items-center justify-between px-5 py-4 font-regular whitespace-nowrap transition-colors ${
+          isActive ? "bg-gray-300 font-semibold text-white" : ""
+        } ${isActive ? "" : "hover:bg-gray-200"}`}
       >
-        <p>
+        <p className="flex items-center">
           {children}
-          {productCnt && <span>({productCnt})</span>}
+          {productCnt && <span className="ml-1">({productCnt})</span>}
         </p>
         {notificationCnt && (
-          <p className="w-5 h-5 text-sm text-center text-white bg-red-500 rounded-full font-extralight">
+          <span className="flex items-center justify-center w-5 h-5 text-sm font-light text-center text-white bg-red-500 rounded-full">
             {notificationCnt}
-          </p>
+          </span>
         )}
       </div>
     </Link>
