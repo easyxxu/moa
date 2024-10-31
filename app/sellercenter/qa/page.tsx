0caa02dee2371f6ddac28366e83e135e41b72725
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-31 16:54:07 +0900

    Style: hover 스타일 추가

--- app/sellercenter/qa/page.tsx
+++ app/sellercenter/qa/page.tsx
@@ -38,11 +38,10 @@ export default async function QADashboard() {
                 </p>
               </td>
               <td className="text-center">
-                <Link
-                  href={`/sellercenter/qa/${product.id}`}
-                  className="px-3 py-2 rounded-xl shadow-out bg-secondary"
-                >
-                  답변등록
+                <Link href={`/sellercenter/qa/${product.id}`}>
+                  <p className="px-3 py-2 text-blue-400 underline transition-transform duration-200 ease-out underline-offset-4 hover:-translate-y-1">
+                    답변등록
+                  </p>
                 </Link>
               </td>
             </tr>
