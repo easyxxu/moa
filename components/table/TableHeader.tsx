ddbb630483932cccc4586599c4148b9974d88789
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-10-31 17:41:32 +0900

    Style: 왼쪽 메뉴와 간격 동일하게 수정

--- components/table/TableHeader.tsx
+++ components/table/TableHeader.tsx
@@ -3,10 +3,10 @@ interface Props {
 }
 export default function TableHeader({ headers }: Props) {
   return (
-    <thead className="text-lg text-black bg-gray-200">
+    <thead className="text-black bg-gray-200">
       <tr>
         {headers.map((header, idx) => (
-          <th className={`py-2 px-4`} key={idx}>
+          <th className={`py-4 px-4`} key={idx}>
             {header}
           </th>
         ))}
