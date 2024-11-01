295706aa1c0bc8b95aec4a35ae5854e026c9d91a
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-01 22:17:43 +0900

    Style: TableHeader 텍스트 스타일 수정

--- components/table/TableHeader.tsx
+++ components/table/TableHeader.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export default function TableHeader({ headers }: Props) {
   return (
-    <thead className="text-black bg-gray-200">
+    <thead className="text-center text-gray-700 bg-gray-200">
       <tr>
         {headers.map((header, idx) => (
           <th className={`py-4 px-4`} key={idx}>
