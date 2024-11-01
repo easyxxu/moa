acfc9e083537a15476d5bde0dc93992eae70b322
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-01 14:35:05 +0900

    Style: 개인정보수정 스타일 수정

--- app/mypage/modify/page.tsx
+++ app/mypage/modify/page.tsx
@@ -1,23 +1,28 @@
-import ChangeMyInfo from "@/components/mypage/myinfo/ChangeMyInfo";
-import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 
-export default async function MyInfoPage() {
-  const supabase = createClient();
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
+export default function MyInfoPage() {
   return (
     <div className="w-full">
-      <h2 className="mb-8 text-center">개인정보수정</h2>
-      <ul className="font-regular border-t border-border-grey [&>li]:border-b [&>li]:py-4 [&>li]:border-border-grey">
-        <li>
-          <Link href="/mypage/modify/email">이메일 변경하기</Link>
+      <h2 className="pb-6 mb-6 text-center">개인정보수정</h2>
+      <span className="my-1 w-full h-[1px] block border-b"></span>
+      <ul className="font-regular [&>li>a]:px-2 [&>li>a]:py-1">
+        <li className="">
+          <Link
+            href="/mypage/modify/email"
+            className="block w-full text-left transition-all duration-200 ease-in-out hover:bg-gray-100 hover:rounded hover:shadow-m"
+          >
+            이메일 변경하기
+          </Link>
+          <span className="my-1 w-full h-[1px] block border-b"></span>
         </li>
-        <li>
-          <Link href="/mypage/modify/myinfo">이름, 전화번호 변경하기</Link>
+        <li className="">
+          <Link
+            href="/mypage/modify/myinfo"
+            className="block w-full text-left transition-all duration-200 ease-in-out hover:bg-gray-100 hover:rounded hover:shadow-m"
+          >
+            이름, 전화번호 변경하기
+          </Link>
+          <span className="my-1 w-full h-[1px] block border-b"></span>
         </li>
       </ul>
     </div>
