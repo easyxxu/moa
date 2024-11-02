56de7766665a5050de251382a36238000fc34775
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-03 00:09:21 +0900

    Style: 문의내역 페이지네이션 및 로딩 UI 추가 #22 - 페이지네이션 적용을 위해 클라이언트 컴포넌트로 변경 - 문의 데이터가 없는 경우 사용자 안내 메시지 표시 - 데이터 로딩 중 로딩 UI 추가

--- app/mypage/qa/page.tsx
+++ app/mypage/qa/page.tsx
@@ -1,62 +1,112 @@
-import { getMyQuestions } from "@/api/apis";
-import { answerTitleMap } from "@/app/sellercenter/qa/[productId]/page";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import Loading from "@/components/common/Loading";
+import Pagination from "@/components/common/Pagination";
+import { Tables } from "@/types/database.types";
+
+import { getMyQuestions } from "@/api/qaApis";
+import { answerTitleMap } from "@/utils/answerTitleMap";
+
+interface QuestionWithProduct extends Tables<"question"> {
+  product: Tables<"product"> | null;
+}
+
+interface Question {
+  questions: QuestionWithProduct[];
+  count: number;
+}
 
 const headers = ["상품정보", "문의내역", "작성날짜", "더보기"];
-export default async function QAPage() {
-  const res = await getMyQuestions();
+
+export default function QAPage() {
+  const [data, setData] = useState<Question>({ questions: [], count: 0 });
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadMyQuestions = async () => {
+    setIsLoading(true);
+    const res = await getMyQuestions(page);
+    if (res.status > 400 && res.status < 500) {
+      throw new Error(res.message);
+    }
+    setData({ questions: res.data?.questions!, count: res.data?.count! });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadMyQuestions();
+  }, [page]);
 
   return (
-    <div className="w-full">
-      <h2 className="mb-6 text-2xl font-bold text-center">상품 Q/A</h2>
-      <table className="w-full text-left border-collapse">
-        <thead>
-          <tr className="bg-gray-200">
-            {headers.map((header) => (
-              <th key={header} className="px-4 py-3 text-center text-gray-700">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {res.data?.map((question) => (
-            <tr key={question.id} className="border-b">
-              <td className="flex items-center px-4 py-4 space-x-4">
-                <Image
-                  src={question.product?.image[0]!}
-                  alt={`${question.product?.name} 이미지`}
-                  width={80}
-                  height={80}
-                  className="rounded-md shadow"
-                />
-                <span className="font-medium">{question.product?.name}</span>
-              </td>
-              <td className="px-4 py-4 text-center ">
-                {answerTitleMap[question?.title!]}
-              </td>
-              <td className="px-4 py-4 text-center ">
-                {new Date(question.created_at).toLocaleDateString()}
-              </td>
-              <td className="px-4 py-4 text-center">
-                <Link href={`/mypage/qa/${question.id}`}>
-                  <p className="px-3 py-2 text-blue-400 underline transition-transform duration-200 ease-out underline-offset-4 hover:-translate-y-1">
-                    더보기
-                  </p>
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {res.data?.length === 0 && (
+    <div className="space-y-4">
+      <h2 className="text-2xl font-bold text-center">상품 Q/A</h2>
+      {data.count === 0 && !isLoading && (
         <div className="flex flex-col items-center mt-8 space-y-2">
           <p className="text-lg text-gray-600">작성된 문의가 없습니다.</p>
           <p className="text-gray-500">
             궁금한 점이 있으시면 문의를 작성해보세요!
           </p>
         </div>
+      )}
+      {!isLoading ? (
+        <>
+          <table className="w-full text-left border-collapse">
+            <thead>
+              <tr className="bg-gray-200">
+                {headers.map((header) => (
+                  <th
+                    key={header}
+                    className="px-4 py-3 text-center text-gray-700"
+                  >
+                    {header}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.questions.map((question) => (
+                <tr key={question.id} className="border-b">
+                  <td className="flex items-center px-4 py-4 space-x-4">
+                    <Image
+                      src={question.product?.image[0]!}
+                      alt={`${question.product?.name} 이미지`}
+                      width={80}
+                      height={80}
+                      className="rounded-md shadow"
+                    />
+                    <span className="font-medium">
+                      {question.product?.name}
+                    </span>
+                  </td>
+                  <td className="px-4 py-4 text-center ">
+                    {answerTitleMap[question?.title!]}
+                  </td>
+                  <td className="px-4 py-4 text-center ">
+                    {new Date(question.created_at).toLocaleDateString()}
+                  </td>
+                  <td className="px-4 py-4 text-center">
+                    <Link href={`/mypage/qa/${question.id}`}>
+                      <p className="px-3 py-2 text-blue-400 underline transition-transform duration-200 ease-out underline-offset-4 hover:-translate-y-1">
+                        더보기
+                      </p>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {data.count > 10 && (
+            <div className="flex justify-center">
+              <Pagination total={data?.count} setPage={setPage} />
+            </div>
+          )}
+        </>
+      ) : (
+        <Loading />
       )}
     </div>
   );
