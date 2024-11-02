5f232d8bc63a713d6af7adb26a676271068528ac
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-03 00:15:48 +0900

    Refactor: 상수, 재사용 함수 유틸 폴더로 이동

---
+++ utils/answerTitleMap.ts
@@ -0,0 +1,5 @@
+export const answerTitleMap: { [key: string]: string } = {
+  PRODUCT: "상품 문의",
+  SHIPPING: "배송 문의",
+  ETC: "기타 문의",
+};
---
+++ utils/constants/errorMessage.ts
@@ -0,0 +1,16 @@
+export const ERROR_MESSAGE = {
+  required: "필수 입력입니다.",
+  emailInvalid: "이메일 형식에 맞지 않는 메일 주소 입니다. 다시 입력해주세요.",
+  emailAlreadyExists: "이미 사용 중인 이메일입니다.",
+  passwordInvalid:
+    "비밀번호는 6글자 이상, 최소 1글자의 영어 대소문자, 숫자, 특수문자가 들어가야 합니다.",
+  phoneInvalid: "전화번호는 숫자만 입력 가능합니다.",
+  serverError: "서버 에러가 발생했습니다. 잠시 후 다시 시도해 주세요.",
+  tooManyRequests:
+    "요청이 많아 에러가 발생했습니다. 잠시 후 다시 시도해 주세요.",
+  loginInvalid: "이메일 또는 비밀번호가 잘못되었습니다. 다시 입력해주세요.",
+  loginUserType: "로그인 유형(판매자 또는 구매자)을 다시 확인해주세요.",
+  getUserError:
+    "로그인 정보를 가져오는 데 실패했습니다. 잠시 후 다시 시도해 주세요.",
+  inputInvalid: "입력한 내용이 형식에 맞지 않습니다.",
+};
