d16aff78c51e2aed268b56d1f6ac7fe78425107c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-06 14:08:47 +0900

    Style: 리뷰 0개인 경우 사용자 안내 메시지, 삭제, 구현 추가 #18

--- components/mypage/review/MyReviews.tsx
+++ components/mypage/review/MyReviews.tsx
@@ -2,6 +2,7 @@ import StarRating from "@/components/common/StarRating";
 import { Tables } from "@/types/database.types";
 import Image from "next/image";
 import Link from "next/link";
+import DeleteButton from "./DeleteButton";
 
 interface ReviewsWithProductInfo extends Tables<"review"> {
   product: {
@@ -20,51 +21,63 @@ interface Props {
 export default async function MyReviews({ reviews }: Props) {
   return (
     <ul className="space-y-6">
-      {reviews.map((review) => (
-        <li
-          key={review.id}
-          className="p-4 transition-shadow duration-300 border border-gray-200 rounded-lg shadow-sm hover:shadow-md"
-        >
-          <p className="mb-2 text-sm font-light text-gray-500">
-            {new Date(review.created_at).toLocaleDateString()}
-          </p>
-          <Link href={`/products/${review.product?.id}`}>
-            <div className="flex gap-4 mb-4">
-              <Image
-                src={review.product?.image[0]!}
-                alt="상품 이미지"
-                width={70}
-                height={70}
-                className="object-cover rounded-lg"
-              />
-              <div className="flex flex-col justify-center">
-                <p className="text-xs text-gray-500">
-                  {review.product?.seller_store}
-                </p>
-                <p className="text-lg font-semibold">{review.product?.name}</p>
+      {reviews?.length === 0 ? (
+        <p className="text-center">주문 내역이 없습니다.</p>
+      ) : (
+        reviews?.map((review) => (
+          <li
+            key={review.id}
+            className="p-4 transition-shadow duration-300 border border-gray-200 rounded-lg shadow-sm hover:shadow-md"
+          >
+            <div>
+              <p className="mb-2 text-sm font-light text-gray-500">
+                {new Date(review.created_at).toLocaleDateString()}
+              </p>
+              <Link href={`/products/${review.product?.id}`}>
+                <div className="flex gap-4 mb-4">
+                  <Image
+                    src={review.product?.image[0]!}
+                    alt="상품 이미지"
+                    width={70}
+                    height={70}
+                    className="object-cover rounded-lg"
+                  />
+                  <div className="flex flex-col justify-center">
+                    <p className="text-xs text-gray-500">
+                      {review.product?.seller_store}
+                    </p>
+                    <p className="text-lg font-semibold">
+                      {review.product?.name}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+              <div className="my-3">
+                <StarRating size={20} rating={review.star_rating} />
               </div>
+              <p className="text-gray-700">{review.content}</p>
+              {review.images?.length! > 0 && (
+                <div className="flex gap-2 mt-4">
+                  {review.images?.map((image, idx) => (
+                    <Image
+                      src={image}
+                      alt="리뷰 이미지"
+                      width={80}
+                      height={80}
+                      key={idx}
+                      className="object-cover rounded-md aspect-square"
+                    />
+                  ))}
+                </div>
+              )}
             </div>
-          </Link>
-          <div className="my-3">
-            <StarRating size={20} rating={review.star_rating} />
-          </div>
-          <p className="text-gray-700">{review.content}</p>
-          {review.images?.length! > 0 && (
-            <div className="flex gap-2 mt-4">
-              {review.images?.map((image, idx) => (
-                <Image
-                  src={image}
-                  alt="리뷰 이미지"
-                  width={80}
-                  height={80}
-                  key={idx}
-                  className="object-cover rounded-md aspect-square"
-                />
-              ))}
+            <div>
+              <Link href={`/mypage/review/modify/${review.id}`}>수정</Link>
+              <DeleteButton reviewId={review.id} />
             </div>
-          )}
-        </li>
-      ))}
+          </li>
+        ))
+      )}
     </ul>
   );
 }
