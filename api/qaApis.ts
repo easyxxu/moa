e4503125739da2c745698f587a7e881a402374e7
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-07 22:30:26 +0900

    Feat: 작성자 문의 GET API

--- api/qaApis.ts
+++ api/qaApis.ts
@@ -4,6 +4,55 @@ import { createClient } from "@/utils/supabase/server";
 
 import { getUserInfo } from "./apis";
 import { ERROR_MESSAGE } from "@/utils/constants/errorMessage";
+
+/**
+ * 작성자의 문의 GET API
+ *
+ * 로그인 사용자 ID를 기반으로 사용자가 작성한 문의 목록을 불러오는 API입니다.
+ * Supabase의 `question` 테이블에서 해당 사용자와 관련된 문의 데이터를 조회합니다.
+ *
+ * @param page - 요청할 문의 데이터의 페이지 번호
+ * @returns 문의 데이터 배열 또는 에러 메시지
+ *
+ * @example
+ * const myQuestions = await getQuestions(page);
+ */
+export const getMyQuestions = async (page: number) => {
+  const supabase = createClient();
+  const start = (page - 1) * 10;
+  const end = start + 9;
+  // 현재 로그인 유저 정보 얻어오기
+  const res = await getUserInfo();
+  if (res.status > 400 && res.status < 500) {
+    throw new Error(res.message);
+  }
+
+  const {
+    data: questions,
+    error,
+    status,
+    count,
+  } = await supabase
+    .from("question")
+    .select(`*, product(*)`, { count: "exact" })
+    .eq("writer_id", res.user?.id!)
+    .range(start, end);
+
+  if (error) {
+    console.error("작성한 문의글을 불러오는 데 실패했습니다.");
+    return { status, message: ERROR_MESSAGE.serverError, error };
+  }
+
+  return {
+    status: 200,
+    message: "작성한 문의글을 불러오는 데 성공했습니다.",
+    data: {
+      questions,
+      count,
+    },
+  };
+};
+
 /**
  * 상품 상세 페이지에서 문의와 답변을 불러오는 API
  */
