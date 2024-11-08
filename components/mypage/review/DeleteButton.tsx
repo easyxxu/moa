8cb18164e8e41a043f517e4b6ba15bb16bcefaf1
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-08 13:29:41 +0900

    Style: 리뷰 수정, 삭제 버튼 스타일링 적용

--- components/mypage/review/DeleteButton.tsx
+++ components/mypage/review/DeleteButton.tsx
@@ -13,7 +13,11 @@ export default function DeleteButton({ reviewId }: Props) {
     }
   };
   return (
-    <button type="button" onClick={handleDelete}>
+    <button
+      type="button"
+      onClick={handleDelete}
+      className="relative px-3 py-2 text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+    >
       삭제
     </button>
   );
--- components/mypage/review/MyReviews.tsx
+++ components/mypage/review/MyReviews.tsx
@@ -71,8 +71,14 @@ export default async function MyReviews({ reviews }: Props) {
                 </div>
               )}
             </div>
-            <div>
-              <Link href={`/mypage/review/modify/${review.id}`}>수정</Link>
+            <div className="flex items-center gap-2 self-start">
+              <Link
+                href={`/mypage/review/modify/${review.id}`}
+                className="relative px-3 py-2 text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+              >
+                수정
+              </Link>
+
               <DeleteButton reviewId={review.id} />
             </div>
           </li>
