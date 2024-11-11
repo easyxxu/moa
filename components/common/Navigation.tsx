4f1b9d2192723a914bc549612e85e97feee121c7
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-11 19:58:49 +0900

    Style: Link Nav 아이템 스타일 수정

--- components/common/Navigation.tsx
+++ components/common/Navigation.tsx
@@ -21,22 +21,22 @@ interface Props {
 
 export default function Navigation({ isLogin, isSeller }: Props) {
   const userDispatch = useUserDispatch();
-
+  const linkCss = "w-12 h-12 flex justify-center rounded-md hover:bg-gray-100";
   return (
     <nav>
-      <ul className="z-20 flex gap-5">
+      <ul className="z-20 flex gap-2">
         {!isLogin ? (
           <>
             <li>
               <ToolTip name="장바구니">
-                <Link href="/cart" className="w-12 h-12 outer-box">
+                <Link href="/cart" className={`${linkCss}`}>
                   <Image src={CartIcon} alt="장바구니" />
                 </Link>
               </ToolTip>
             </li>
             <li>
               <ToolTip name="로그인">
-                <Link href="/login" className="w-12 h-12 outer-box">
+                <Link href="/login" className={`${linkCss}`}>
                   <Image src={PersonIcon} alt="로그인" />
                 </Link>
               </ToolTip>
@@ -48,21 +48,21 @@ export default function Navigation({ isLogin, isSeller }: Props) {
               <>
                 <li>
                   <ToolTip name="장바구니">
-                    <Link href="/cart" className="w-12 h-12 outer-box">
+                    <Link href="/cart" className={`${linkCss}`}>
                       <Image src={CartIcon} alt="장바구니" />
                     </Link>
                   </ToolTip>
                 </li>
                 <li>
                   <ToolTip name="마이페이지">
-                    <Link href="/mypage" className="w-12 h-12 outer-box">
+                    <Link href="/mypage" className={`${linkCss}`}>
                       <Image src={PersonIcon} alt="마이페이지" />
                     </Link>
                   </ToolTip>
                 </li>
                 <li>
                   <ToolTip name="나의 좋아요">
-                    <Link href="/like" className="w-12 h-12 outer-box">
+                    <Link href="/like" className={`${linkCss}`}>
                       <Image src={LikeIcon} alt="좋아요" />
                     </Link>
                   </ToolTip>
@@ -71,6 +71,7 @@ export default function Navigation({ isLogin, isSeller }: Props) {
                   <ToolTip name="로그아웃">
                     <Button
                       type="button"
+                      style="none"
                       custom="w-12 h-12"
                       onClick={async () => {
                         await logOutAction();
@@ -90,6 +91,7 @@ export default function Navigation({ isLogin, isSeller }: Props) {
                   <ToolTip name="로그아웃">
                     <Button
                       type="button"
+                      style="none"
                       custom="w-12 h-12"
                       onClick={async () => {
                         await logOutAction();
@@ -104,7 +106,7 @@ export default function Navigation({ isLogin, isSeller }: Props) {
                 </li>
                 <li>
                   <ToolTip name="판매자센터">
-                    <Link href="/sellercenter" className="w-12 h-12 outer-box">
+                    <Link href="/sellercenter" className={`${linkCss}`}>
                       <Image src={ShopIcon} alt="판매자샵" />
                     </Link>
                   </ToolTip>
