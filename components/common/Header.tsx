c65e63cfff83380dcc931fc8b6f6799830002582
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-11 17:43:18 +0900

    Style: 판매자센터인 경우 헤더 sticky 적용 해제

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -19,9 +19,9 @@ export default function Header() {
     userState.userType === "SELLER" && currentPath.includes("/sellercenter");
   // console.log(userState);
   return (
-    <header className="sticky top-0 z-30 bg-white">
+    <>
       {!isSellerCenter ? (
-        <>
+        <header className="sticky top-0 z-30 bg-white">
           <div className="flex items-center justify-between px-2 py-5 mx-auto my-0 max-w-7xl">
             <div className="flex items-center gap-9">
               <h1>
@@ -37,17 +37,23 @@ export default function Header() {
             </div>
             <Navigation isLogin={isLogin} isSeller={isSeller} />
           </div>
-        </>
+        </header>
       ) : (
-        <div className="flex items-center gap-4 py-5 mx-auto my-0 max-w-7xl">
-          <h1>
-            <Link href="/">
-              <Image src={MoaLogo} alt="모아 로고" className="w-32 min-w-16" />
-            </Link>
-          </h1>
-          <h2 className="text-4xl text-black">판매자센터</h2>
-        </div>
+        <header>
+          <div className="flex items-center gap-4 py-5 mx-auto my-0 max-w-7xl">
+            <h1>
+              <Link href="/">
+                <Image
+                  src={MoaLogo}
+                  alt="모아 로고"
+                  className="w-32 min-w-16"
+                />
+              </Link>
+            </h1>
+            <h2 className="text-4xl text-black">판매자센터</h2>
+          </div>
+        </header>
       )}
-    </header>
+    </>
   );
 }
