3ab7e241a7f0ecf3b2be3acdd7c5556e5a664a83
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-13 23:58:36 +0900

    Style: 이미지 alt 수정

--- components/authForm/OAuthButton.tsx
+++ components/authForm/OAuthButton.tsx
@@ -41,7 +41,7 @@ export default function OAuthButton({ formType }: Props) {
         onClick={() => handleSignInWithOAuth("google")}
         className="px-3 py-2 w-full flex items-centner rounded-sm border text-[#80868B] bg-[#FFFFFF]"
       >
-        <Image src={GoogleIcon} alt="카카오 로고" width={18} height={18} />
+        <Image src={GoogleIcon} alt="구글 로고" width={18} height={18} />
         <span className="flex-grow text-center">
           구글로 {formType === "join" ? "시작하기" : "로그인"}
         </span>
