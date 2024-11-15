0f1259b313944f06e26bd9e8a26df44e96804753
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-15 11:18:56 +0900

    Feat: 상품 필터링을 위한 옵션 작성 #24

---
+++ utils/constants/filterOptions.ts
@@ -0,0 +1,22 @@
+export const CHARACTER_OPTIONS = [
+  { name: "chiikawa", text: "치이카와" },
+  { name: "sanrio", text: "산리오" },
+  { name: "jjanggu", text: "짱구" },
+  { name: "other", text: "기타" },
+];
+
+export const CATEGORY_OPTIONS = [
+  { name: "all", text: "전체" },
+  { name: "doll", text: "인형" },
+  { name: "gacha", text: "가챠" },
+  { name: "rement", text: "리멘트" },
+  { name: "other", text: "기타" },
+];
+
+export const ORDER_OPTIONS = [
+  { name: "mostOrders", text: "주문많은순" },
+  { name: "latest", text: "최신순" },
+  { name: "mostLikes", text: "좋아요많은순" },
+  { name: "highestPrice", text: "높은가격순" },
+  { name: "lowestPrice", text: "낮은가격순" },
+];
