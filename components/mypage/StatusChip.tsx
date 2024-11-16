c3695f89cd0e6b1ce803fc29e444a72d35549958
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-16 16:22:41 +0900

    Style: status 조건부 로직을 statusMap 사용으로 변경

--- components/mypage/StatusChip.tsx
+++ components/mypage/StatusChip.tsx
@@ -1,12 +1,19 @@
-import { OrderStatus } from "@/types/order";
-
 interface Props {
-  status: OrderStatus;
+  status: string;
 }
+
+const statusMap: Record<string, string> = {
+  PAYMENT_PENDING: "결제대기중",
+  PAYMENT_COMPLETED: "결제완료",
+  PAYMENT_FAILED: "결제실패",
+  PAYMENT_CANCELED: "결제취소됨",
+  PAYMENT_REFUNDED: "결제환불됨",
+};
+
 export default function StatusChip({ status }: Props) {
   return (
-    <div className="font-light text-font-grey">
-      <p>{status === "PAYMENT_PENDING" ? "결제대기" : "결제완료"}</p>
+    <div className="py-1 font-light border border-gray-400 rounded-sm text-font-grey">
+      <p>{statusMap[status]}</p>
     </div>
   );
 }
