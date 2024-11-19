035a0b1ee08f17801e3b44988ce05b7f74fee382
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-19 14:06:02 +0900

    Style: 판매자센터 통계 페이지 #7

--- app/sellercenter/analytics/page.tsx
+++ app/sellercenter/analytics/page.tsx
@@ -1,3 +1,30 @@
-export default function AnalyticsDashboard() {
-  return <h3>Analytics</h3>;
+import { getOrderDataWithChartFormat } from "@/api/chartApis";
+import { BarChart, LineChart } from "@/components/sellercenter/Chart";
+
+export default async function AnalyticsDashboard() {
+  const { status, message, data } = await getOrderDataWithChartFormat();
+  if ((status >= 400 && status < 500) || !data) {
+    throw new Error(message);
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full gap-6 px-4 my-6 mb-10">
+        <div className="w-3/4">
+          <LineChart
+            chartTitle="매출현황"
+            labels={data.labels}
+            totalPriceData={data.totalPriceData}
+          />
+        </div>
+        <div className="w-3/4">
+          <BarChart
+            chartTitle="주문수"
+            labels={data.labels}
+            totalCountData={data.totalCountData}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
