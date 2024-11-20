e203ef81d64924f0507799ecaae91cbf03d5a57f
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-20 11:15:30 +0900

    Feat: 배포 url 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ToastProvider } from "@/contexts/toastContext";
 
 export const defaultUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}`
+    ? `${process.env.VERCEL_URL}`
     : "http://localhost:3000";
 
 export const metadata = {
