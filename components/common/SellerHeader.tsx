5bedf0509c567fe380c725087dc45f83067339a1
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-22 00:04:53 +0900

    Style: 현재 어떤 판매자로 로그인했는지 알 수 있도록 SellerHeader에 추가

--- components/common/SellerHeader.tsx
+++ components/common/SellerHeader.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-
+import { useUserState } from "@/contexts/UserContext";
+import ShopIcon from "@/public/assets/icon/icon-seller-shop.svg";
+import Image from "next/image";
 const HeaderPath: Record<string, string> = {
   "/sellercenter/product": "상품관리",
   "/sellercenter/order": "주문관리",
@@ -13,7 +15,7 @@ const HeaderPath: Record<string, string> = {
 
 export default function SellerHeader() {
   const currentPath = usePathname();
-
+  const userInfo = useUserState();
   const headerText = (() => {
     for (const [path, label] of Object.entries(HeaderPath).sort(
       (a, b) => b[0].length - a[0].length
@@ -24,8 +26,12 @@ export default function SellerHeader() {
   })();
 
   return (
-    <header className="w-full px-4 py-3 text-2xl font-semibold border-b">
+    <header className="flex justify-between w-full px-4 py-3 text-2xl font-semibold border-b">
       <p>{headerText}</p>
+      <div className="flex items-center gap-1">
+        <Image src={ShopIcon} alt="샵 아이콘" width={30} height={30} />
+        <p className="text-lg">{userInfo.name}</p>
+      </div>
     </header>
   );
 }
