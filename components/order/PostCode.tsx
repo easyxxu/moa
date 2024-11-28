1c6bff06034b210a9202788fdbb7c0c5483d0e99
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-11-28 15:04:40 +0900

    Refactor: setState가 아닌 handler를 통해 주소 정보를 저장할 수 있도록 변경 #14

--- components/order/PostCode.tsx
+++ components/order/PostCode.tsx
@@ -1,40 +1,18 @@
 import { CartItem } from "@/contexts/CartContext";
+import { OrderItem } from "@/contexts/DirectOrderContext";
 import { CSSProperties } from "react";
 import DaumPostCode from "react-daum-postcode";
 import { Address as DaumAddress } from "react-daum-postcode/lib/loadPostcode";
 
-interface FormData {
-  name: string;
-  email: string;
-  phone: string;
-  postCode: string;
-  defaultAddress: string;
-  detailAddress: string;
-  deliveryMessage: string;
-  orderItems: CartItem[];
-  totalPrice: number;
-  orderName: string;
-  payment: string;
-}
-
 interface PostCodeProps {
-  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+  handleAddress: (postCode: string, defaultAddress: string) => void;
   setPostCodeModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function PostCode({
-  setFormData,
+  handleAddress,
   setPostCodeModal,
 }: PostCodeProps) {
-  const handleComplete = (data: DaumAddress) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      postCode: data.zonecode,
-      defaultAddress: data.roadAddress,
-    }));
-    setPostCodeModal(false);
-  };
-
   const handleClickOutSide = () => {
     setPostCodeModal(false);
   };
@@ -54,7 +32,13 @@ export default function PostCode({
       onClick={handleClickOutSide}
       className="fixed w-screen h-screen top-0 left-0 bg-[rgba(0,0,0,0.5)] z-10"
     >
-      <DaumPostCode onComplete={handleComplete} style={postCodeStyle} />
+      <DaumPostCode
+        onComplete={(data: DaumAddress) => {
+          handleAddress(data.zonecode, data.roadAddress);
+          setPostCodeModal(false);
+        }}
+        style={postCodeStyle}
+      />
     </div>
   );
 }
