023fffe04312736ec6e63a3d2ce56e270d7e7795
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-12-05 17:48:13 +0900

    Fix: useSearchParams 에러 수정 - Suspense 적용

--- app/mypage/modify/email/page.tsx
+++ app/mypage/modify/email/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 import { updateEmail } from "@/api/userApis";
 import Button from "@/components/common/button/Button";
@@ -60,40 +60,42 @@ export default function ModifyEmail() {
     verifyEmail();
   }, [searchParams]);
   return (
-    <div className="w-full">
-      <h2 className="mb-6 text-center">이메일 변경</h2>
-      <form action={formAction}>
-        <div className="flex flex-col gap-2">
-          <InputLabel
-            type="text"
-            fieldId="email"
-            fieldName="email"
-            labelText="현재 이메일"
-            style="box"
-            value={nowEmail}
-            custom="flex-grow"
-            readOnly
-          />
-          <InputLabel
-            type="text"
-            fieldId="newEmail"
-            fieldName="newEmail"
-            labelText="새 이메일"
-            style="box"
-            custom="flex-grow"
-            // error={(state.error as ErrorMsg)?.email}
-          />
-          {/* <SubmitButton /> */}
-          <Button
-            type="submit"
-            style="point"
-            disable={pending}
-            custom={`py-2 ${pending ? "bg-gray-300" : ""}`}
-          >
-            {pending ? "이메일 인증을 해주세요." : "변경하기"}
-          </Button>
-        </div>
-      </form>
-    </div>
+    <Suspense>
+      <div className="w-full">
+        <h2 className="mb-6 text-center">이메일 변경</h2>
+        <form action={formAction}>
+          <div className="flex flex-col gap-2">
+            <InputLabel
+              type="text"
+              fieldId="email"
+              fieldName="email"
+              labelText="현재 이메일"
+              style="box"
+              value={nowEmail}
+              custom="flex-grow"
+              readOnly
+            />
+            <InputLabel
+              type="text"
+              fieldId="newEmail"
+              fieldName="newEmail"
+              labelText="새 이메일"
+              style="box"
+              custom="flex-grow"
+              // error={(state.error as ErrorMsg)?.email}
+            />
+            {/* <SubmitButton /> */}
+            <Button
+              type="submit"
+              style="point"
+              disable={pending}
+              custom={`py-2 ${pending ? "bg-gray-300" : ""}`}
+            >
+              {pending ? "이메일 인증을 해주세요." : "변경하기"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </Suspense>
   );
 }
