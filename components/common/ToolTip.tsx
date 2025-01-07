15eabc20c49f5046612dc1c73c31f82eab563ce3
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-01-07 16:25:12 +0900

    Style: ToolTip z-index 추가

--- components/common/ToolTip.tsx
+++ components/common/ToolTip.tsx
@@ -6,7 +6,7 @@ export default function ToolTip({ children, name }: Props) {
   return (
     <div className="relative group">
       {children}
-      <div className="invisible text-gray-50 absolute translate-y-[120%] -translate-x-[50%] left-[50%] -bottom-0 whitespace-nowrap px-3 py-1 opacity-80 rounded-3xl bg-black group-hover:visible">
+      <div className="z-30 invisible text-gray-50 absolute translate-y-[120%] -translate-x-[50%] left-[50%] -bottom-0 whitespace-nowrap px-3 py-1 opacity-80 rounded-3xl bg-black group-hover:visible">
         <p className="text-sm font-light">{name}</p>
       </div>
     </div>
