11a87db2a6140442f29307752860a1ffdd6104bc
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-01-14 15:06:06 +0900

    Style: 주문상세페이지 반응형 #27 - 각 섹션의 간격 수정 - 주문자 정보 테이블 className 중복 제거 및 상수화

--- app/mypage/order/[orderId]/page.tsx
+++ app/mypage/order/[orderId]/page.tsx
@@ -5,6 +5,8 @@ import TableHeader from "@/components/table/TableHeader";
 import { createClient } from "@/utils/supabase/server";
 
 const HEADER_TITLES = ["주문정보", "배송비", "주문상태", "리뷰작성"];
+const ORDERER_INFO_TH_STYLE =
+  "w-1/4 px-4 py-2 text-gray-800 border-r border-gray-900 font-normal";
 
 export default async function OrderDetailPage({
   params,
@@ -35,10 +37,12 @@ export default async function OrderDetailPage({
     return;
   }
   return (
-    <div className="flex flex-col w-full gap-6 ">
+    <div className="px-2 flex flex-col w-full gap-6 ">
       {/* 주문 상세 정보 */}
-      <section className="pb-4 border-b-2">
-        <h3 className="mb-4 text-2xl font-semibold">주문상세내역</h3>
+      <section className="space-y-2">
+        <h3 className="pb-2 text-2xl font-semibold border-b-4 border-gray-900">
+          주문상세내역
+        </h3>
         <p className="text-sm text-gray-700">
           주문일자{" "}
           <span className="font-medium">
@@ -50,84 +54,76 @@ export default async function OrderDetailPage({
       </section>
 
       {/* 주문 상품 정보 */}
-      <section className="pb-4 border-b-2">
-        <h3 className="mb-4 text-2xl font-semibold">주문상품정보</h3>
-        <table className="w-full">
-          <TableHeader headers={HEADER_TITLES} />
-          <tbody>
-            {order.order_item?.map((item) => (
-              <tr key={item.id} className="border-b">
-                <td className="px-4 py-4">
-                  <Link href={`/products/${item.item_id}`}>
-                    <OrderProductItem
-                      image={item.product?.image[0]!}
-                      name={item.product?.name!}
-                      price={item.price}
-                      seller_store={item.product?.seller_store!}
-                      quantity={item.quantity}
-                    />
-                  </Link>
-                </td>
-                <td className="px-4 py-4 text-center">
-                  {item.shipping_fee.toLocaleString()} 원
-                </td>
-                <td className="px-4 py-4 text-center">
-                  <StatusChip status={order?.order_status!} />
-                </td>
-                <td className="px-4 py-4 text-center">
-                  {item.review_status ? (
-                    "작성완료"
-                  ) : (
-                    <Link
-                      href={`/mypage/review/write/${item.id}`}
-                      className="text-blue-500 underline hover:text-blue-600"
-                    >
-                      작성하기
+      <section className="space-y-2">
+        <h4 className="text-xl font-semibold">주문상품정보</h4>
+        <div className="border-b border-gray-900 ">
+          <table className="w-full">
+            <TableHeader headers={HEADER_TITLES} />
+            <tbody>
+              {order.order_item?.map((item) => (
+                <tr key={item.id} className="border-b">
+                  <td className="px-4 py-4">
+                    <Link href={`/products/${item.item_id}`}>
+                      <OrderProductItem
+                        image={item.product?.image[0]!}
+                        name={item.product?.name!}
+                        price={item.price}
+                        seller_store={item.product?.seller_store!}
+                        quantity={item.quantity}
+                      />
                     </Link>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <p className="mt-4 text-lg font-semibold text-right">
+                  </td>
+                  <td className="px-4 py-4 text-center">
+                    {item.shipping_fee.toLocaleString()} 원
+                  </td>
+                  <td className="px-4 py-4 text-center">
+                    <StatusChip status={order?.order_status!} />
+                  </td>
+                  <td className="px-4 py-4 text-center">
+                    {item.review_status ? (
+                      "작성완료"
+                    ) : (
+                      <Link
+                        href={`/mypage/review/write/${item.id}`}
+                        className="text-blue-500 underline hover:text-blue-600"
+                      >
+                        작성하기
+                      </Link>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <p className="text-lg font-semibold text-right">
           총 결제금액: {order.total_price.toLocaleString()} 원
         </p>
       </section>
 
       {/* 주문자 정보 */}
-      <section>
-        <h3 className="mb-4 text-2xl font-semibold">주문자정보</h3>
-        <table className="w-full text-left border border-gray-300">
+      <section className="space-y-2">
+        <h4 className="text-xl font-semibold">주문자정보</h4>
+        <table className="w-full text-left border border-gray-900">
           <tbody>
             <tr>
-              <th className="w-1/4 px-4 py-2 text-gray-700 bg-gray-100">
-                이름
-              </th>
+              <th className={`${ORDERER_INFO_TH_STYLE}`}>이름</th>
               <td className="px-4 py-2">{order.customer_name}</td>
             </tr>
             <tr>
-              <th className="w-1/4 px-4 py-2 text-gray-700 bg-gray-100">
-                전화번호
-              </th>
+              <th className={`${ORDERER_INFO_TH_STYLE}`}>전화번호</th>
               <td className="px-4 py-2">{order.customer_phone}</td>
             </tr>
             <tr>
-              <th className="w-1/4 px-4 py-2 text-gray-700 bg-gray-100">
-                이메일
-              </th>
+              <th className={`${ORDERER_INFO_TH_STYLE}`}>이메일</th>
               <td className="px-4 py-2">{order.customer_email}</td>
             </tr>
             <tr>
-              <th className="w-1/4 px-4 py-2 text-gray-700 bg-gray-100">
-                주소
-              </th>
+              <th className={`${ORDERER_INFO_TH_STYLE}`}>주소</th>
               <td className="px-4 py-2">{order.customer_address}</td>
             </tr>
             <tr>
-              <th className="w-1/4 px-4 py-2 text-gray-700 bg-gray-100">
-                배송메세지
-              </th>
+              <th className={`${ORDERER_INFO_TH_STYLE}`}>배송메세지</th>
               <td className="px-4 py-2">
                 {order.customer_delivery_message?.length === 0
                   ? "-"
