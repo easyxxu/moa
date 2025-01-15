0fd0ba70af318be32446561f5b4c9e601f58813c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-01-15 22:45:02 +0900

    Style: 지원하지 않는 기능 알림 컴포넌트 높이 수정

--- components/common/NotAvailableYet.tsx
+++ components/common/NotAvailableYet.tsx
@@ -1,6 +1,6 @@
 export default function NotAvailableYet() {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-56">
       아직 이용할 수 없어요.🥲 빠른 시일 내에 제공할 수 있도록 하겠습니다!
     </div>
   );
