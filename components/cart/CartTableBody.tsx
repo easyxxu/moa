fe538ce1a588a5d339ced6527097c2463d14dea3
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-01-16 11:51:59 +0900

    Style: key prop 추가

--- components/cart/CartTableBody.tsx
+++ components/cart/CartTableBody.tsx
@@ -2,6 +2,7 @@
 
 import { CartItemInfo, GroupedCartItems } from "@/types/cart";
 import CartTableItem from "./CartTableItem";
+import { Fragment } from "react";
 
 interface Props {
   cartItems: GroupedCartItems;
@@ -11,8 +12,8 @@ export default function CartTableBody({ cartItems }: Props) {
   return (
     <tbody className="text-center">
       <tr className="h-5" />
-      {Object.keys(cartItems).map((store) => (
-        <>
+      {Object.keys(cartItems).map((store, idx) => (
+        <Fragment key={idx}>
           <tr className="bg-white">
             <td colSpan={5} className="p-3 font-bold text-left">
               {store}
@@ -20,10 +21,14 @@ export default function CartTableBody({ cartItems }: Props) {
           </tr>
           {cartItems[store].map((item: CartItemInfo, index: number) => {
             const isLastItem = index === cartItems[store].length - 1;
-            return <CartTableItem item={item} isLastItem={isLastItem} />;
+            return (
+              <Fragment key={index}>
+                <CartTableItem item={item} isLastItem={isLastItem} />
+              </Fragment>
+            );
           })}
           <tr className="h-5" />
-        </>
+        </Fragment>
       ))}
     </tbody>
   );
