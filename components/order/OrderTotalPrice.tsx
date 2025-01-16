84d6232cfd1def49a2bab8d06fb23cad436ec4d8
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-01-16 17:31:10 +0900

    Style: 금액 표현 방식 수정 - 콜론 제거

--- components/order/OrderTotalPrice.tsx
+++ components/order/OrderTotalPrice.tsx
@@ -13,10 +13,10 @@ export default function OrderTotalPrice() {
   return (
     <div className="flex justify-end gap-4 px-10 py-2 border-gray-900 border-y">
       <div className="flex gap-4">
-        <p>상품금액: {price.productsPrice.toLocaleString()} 원</p>+
-        <p>배송비: {price.shippingFee.toLocaleString()} 원</p>
+        <p>상품금액 {price.productsPrice.toLocaleString()}원</p>+
+        <p>배송비 {price.shippingFee.toLocaleString()}원</p>
       </div>
-      =<p>총 결제 금액: {price.totalPrice.toLocaleString()} 원</p>
+      =<p>총 결제 금액 {price.totalPrice.toLocaleString()}원</p>
     </div>
   );
 }
