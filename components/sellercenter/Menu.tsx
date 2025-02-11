e75c155356a12eec2cd8a0a5282bc552f45df780
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-02-11 22:59:22 +0900

    Style: Menu 수정 - 모아 로고, 로그아웃 버튼 ul 내부로 이동

--- components/sellercenter/Menu.tsx
+++ components/sellercenter/Menu.tsx
@@ -22,18 +22,20 @@ const MENU_ITEMS = [
   { title: "통계", icon: ChartIcon, path: "/sellercenter/analytics" },
   { title: "스토어 설정", icon: ShopIcon, path: "/sellercenter/setting" },
 ];
+
 export default function Menu() {
   const userDispatch = useUserDispatch();
+
   return (
-    <nav className="flex flex-col justify-between h-full overflow-y-hidden bg-white border-r">
-      <div>
-        <Image
-          src={MoALogo}
-          alt="모아로고"
-          height={50}
-          className="w-3/4 px-3 py-4 mx-auto"
-        />
-        <ul className="flex flex-col gap-2 px-2">
+    <nav className="h-full bg-white border-t sm:border-r">
+      <ul className="flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-2 px-2">
+          <Image
+            src={MoALogo}
+            alt="모아로고"
+            height={50}
+            className="w-full px-3 py-4 max-w-24"
+          />
           {MENU_ITEMS.map((item, idx) => (
             <li
               key={idx}
@@ -54,23 +56,23 @@ export default function Menu() {
               </Link>
             </li>
           ))}
-        </ul>
-      </div>
-      <div className="px-2 pb-2">
-        <button
-          type="button"
-          className="flex items-center gap-2 px-2 py-3 transition-colors duration-200 rounded-sm hover:bg-gray-200"
-          onClick={() => userDispatch?.logout()}
-        >
-          <Image
-            src={LogoutIcon}
-            alt="로그아웃 아이콘"
-            width={30}
-            height={30}
-          />
-          <span className="pr-6 text-nowrap">로그아웃</span>
-        </button>
-      </div>
+          <li>
+            <button
+              type="button"
+              className="flex w-full items-center gap-2 px-2 py-3 transition-colors duration-200 rounded-sm hover:bg-gray-200"
+              onClick={() => userDispatch?.logout()}
+            >
+              <Image
+                src={LogoutIcon}
+                alt="로그아웃 아이콘"
+                width={30}
+                height={30}
+              />
+              <span className="pr-6 text-nowrap">로그아웃</span>
+            </button>
+          </li>
+        </div>
+      </ul>
     </nav>
   );
 }
