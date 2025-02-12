7f97b7fd91b1f07b6966566a058cd3bf4238cb4d
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-02-12 17:34:01 +0900

    Fix: 잘못된 배송비 값 수정

--- components/cart/MobileCartTotalPrice.tsx
+++ components/cart/MobileCartTotalPrice.tsx
@@ -8,7 +8,7 @@ export default function MobileCartTotalPrice() {
     <div className="flex flex-col gap-3 px-2 text-lg sm:hidden">
       <PriceItem priceType="총 상품 금액" price={price.productsPrice} />
       <PriceItem priceType="상품 할인" price={0} />
-      <PriceItem priceType="배송비" price={price.productsPrice} />
+      <PriceItem priceType="배송비" price={price.shippingFee} />
       <PriceItem priceType="결제 예정 금액" price={price.totalPrice} />
     </div>
   );
