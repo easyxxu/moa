eee1f5d6adb7ac4cfc1ab568d303e194d9e09612
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-02-12 23:28:26 +0900

    Style: text-nowrap 적용

--- components/mypage/review/DeleteButton.tsx
+++ components/mypage/review/DeleteButton.tsx
@@ -20,7 +20,7 @@ export default function DeleteButton({ reviewId }: Props) {
     <button
       type="button"
       onClick={handleDelete}
-      className="relative px-3 py-2 text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+      className="relative px-3 text-nowrap py-2 text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
     >
       삭제
     </button>
--- components/mypage/review/MyReviews.tsx
+++ components/mypage/review/MyReviews.tsx
@@ -55,7 +55,7 @@ export default async function MyReviews({ reviews }: Props) {
               <div className="my-3">
                 <StarRating size={20} rating={review.star_rating} />
               </div>
-              <p className="text-gray-700">{review.content}</p>
+              <p>{review.content}</p>
               {review.images?.length! > 0 && (
                 <div className="flex gap-2 mt-4">
                   {review.images?.map((image, idx) => (
@@ -74,7 +74,7 @@ export default async function MyReviews({ reviews }: Props) {
             <div className="flex items-center self-start gap-2">
               <Link
                 href={`/mypage/review/modify/${review.id}`}
-                className="relative px-3 py-2 text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+                className="relative px-3 py-2 text-nowrap text-sm font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
               >
                 수정
               </Link>
