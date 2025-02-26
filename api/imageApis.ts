041fcf0c25aebd7c92ca93cb0641e5b80aa5ec55
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-02-26 15:03:17 +0900

    Refactor: 이미지 업로드 병렬 처리 - map() + Promise.all을 사용하여 병렬 처리하도록 변경

--- api/imageApis.ts
+++ api/imageApis.ts
@@ -1,28 +1,29 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { v4 } from "uuid";
 
 export const uploadImgs = async (files: File[], fileCategory: string) => {
   const supabase = createClient();
-  const fileFolder = crypto.randomUUID();
-  const uploadedImgUrls = [];
+  const folderId = v4();
+
   try {
-    for (let i = 0; i < files.length; i++) {
-      // 이미지 업로드
+    const uploadPromises = files.map(async (file, index) => {
+      const filePath = `${fileCategory}/${folderId}/${index}`;
+
       const { data, error } = await supabase.storage
         .from("Image")
-        .upload(`/${fileCategory}/${fileFolder}/${i}`, files[i]);
-      if (error) {
-        throw { message: "이미지 업로드 실패", error };
-      }
-      // 이미지 url 로드
+        .upload(filePath, file);
+
+      if (error) throw new Error(`이미지 업로드에 실패했습니다. ${error}`);
+
       const {
         data: { publicUrl },
       } = await supabase.storage.from("Image").getPublicUrl(data.path);
-      uploadedImgUrls.push(publicUrl);
-    }
+      return publicUrl;
+    });
 
-    return uploadedImgUrls;
+    return await Promise.all(uploadPromises);
   } catch (e) {
     console.error("[ERROR] ", e);
     throw new Error("서버에서 문제가 생겼습니다.");
